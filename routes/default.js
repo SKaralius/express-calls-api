546888cc690f82ce26e490a8a60b0c163ac6d8cc
SKaralius <k.simonas.k@gmail.com>
2021-02-21 13:02:19 +0200

    Calls API.

--- routes/default.js
+++ routes/default.js
@@ -1,9 +1,20 @@
 import express from "express";
+import fetch from "node-fetch";
 
 const router = express.Router();
 
-router.use("/getPriceHistory", async (req, res) => {
-    res.send("Hello world");
+router.use("/getPriceHistory/:startDate/:endDate", async (req, res) => {
+    const { startDate, endDate } = req.params;
+    // TODO: Error handling, what if params are not dates?
+    console.log(startDate, endDate);
+    const response = await fetch(
+        `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+    );
+    const responseJson = await response.json();
+    for (let date in responseJson.bpi) {
+        responseJson.bpi[date] *= 1000;
+    }
+    res.json(responseJson);
 });
 
 export default router;
